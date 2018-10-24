4eb2a365dc813d8b7cae682b68ae0f88e755caef
alumno informatica <alumno@ciis.usil.edu.pe>
2018-10-24 08:37:16 -0500

    Interfaz

--- programa.py
+++ programa.py
@@ -1,1 +1,16 @@
-tgutff
+from tkinter import *
+
+raiz=Tk()
+raiz.title("Proyecto")
+raiz.resizable(False,True)
+raiz.geometry("650x350")
+raiz.config(bg="white")
+raiz.mainloop()
+
+root=Tk()
+miFrame = Frame(root, width=500, heigth=400)
+miFrame.pack()
+miLabel=Label(miFrame, text="Bebidas")
+miLabel.pack()
+
+root.mainloop()
