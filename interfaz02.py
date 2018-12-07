66ec469072b41fc43dc1d4fcdcbe3a5564d24cec
CesarAdria21 <44200993+CesarAdria21@users.noreply.github.com>
2018-12-07 16:49:20 -0500

    Update interfaz02.py

--- interfaz02.py
+++ interfaz02.py
@@ -28,3 +28,115 @@ botonCliente = Button(raiz,text="   Cliente   ",command=codigoAdministrador)
 botonCliente.place(x=130,y=60)
 
 raiz.mainloop()
+package Archivos;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class EscribeArchivo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nombreArchivo = "texto.txt";
+		
+		try {
+			
+			FileWriter fw = new FileWriter(nombreArchivo);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			bw.write("Que facilito es...");
+			bw.newLine();
+			bw.write("es escribir archivos!");
+			
+			bw.close();
+			
+			System.out.println("Reto cumplido");
+			
+	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+package Archivos;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class EscribeArchivo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nombreArchivo = "texto.txt";
+		
+		try {
+			
+			FileWriter fw = new FileWriter(nombreArchivo);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			bw.write("Que facilito es...");
+			bw.newLine();
+			bw.write("es escribir archivos!");
+			
+			bw.close();
+			
+			System.out.println("Reto cumplido");	
+	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+----------------------
+private void jBLeerActionPerformed(java.awt.event.ActionEvent evt) {                                       
+        // TODO add your handling code here:
+        
+        String nombre = JTNombreArchivo.getText();
+        
+        try {
+            FileReader fr = new FileReader(nombre);
+            
+            BufferedReader br = new BufferedReader(fr);
+            
+            String line="";
+            String tota="";
+            while((line=br.readLine())!=null){
+                tota+=line+"\n"; // por cada linea leida agregamos un salto de linea
+            }
+
+            jTextArchivo.setText(tota);
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JFArchivo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(JFArchivo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }                                      
+
+    private void jBEscribirActionPerformed(java.awt.event.ActionEvent evt) {                                           
+        // TODO add your handling code here:
+        
+        String texto = JTNombreArchivo.getText();
+        
+        try {
+            FileWriter fw = new FileWriter(texto);
+            BufferedWriter bw = new BufferedWriter(fw);
+            
+            bw.write(jTextArchivo.getText());
+            
+            bw.close();
+            
+           
+        } catch (IOException ex) {
+            Logger.getLogger(JFArchivo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }  
